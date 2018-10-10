dfb4a4c0d4ed15234eae43c08ea324fbb17597c2
leonluo <coder.leonluo@gmail.com>
2018-10-10 11:23:55 +0800

    initialize

---
+++ Hive/Classes/TodoList/Controller/NoteVC.swift
@@ -0,0 +1,35 @@
+//
+//  NoteVC.swift
+//  Hive
+//
+//  Created by Leon Luo on 3/30/18.
+//  Copyright © 2018 leonluo. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
